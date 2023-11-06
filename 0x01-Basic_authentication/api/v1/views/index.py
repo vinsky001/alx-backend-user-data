9aaa563a916d563ee3bf182262d7ce07ae068c66
vinsky001 <ekibet544@gmail.com>
2023-11-06 20:52:45 +0000

    Added a new endpoint in api/v1/views/index.py  for testing this new error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,15 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
+    Return:
+      - Forbidden error.
+    """
+    abort(403)
+
+
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> None:
     """GET /api/v1/unauthorized
