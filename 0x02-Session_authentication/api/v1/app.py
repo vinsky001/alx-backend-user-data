89f09b7cfe3f4319bdf055009d28b5ceb2ad0b64
vinsky001 <ekibet544@gmail.com>
2023-11-09 10:03:54 +0000

    App.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
+"""Route module for the API.
 """
-Route module for the API
-"""
+import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+
+from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -20,6 +21,22 @@ if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
+if auth_type == 'session_auth':
+    auth = SessionAuth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """Not found handler.
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler.
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
@@ -27,19 +44,6 @@ def forbidden(error) -> str:
     """Forbidden handler.
     """
     return jsonify({"error": "Forbidden"}), 403
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Unathourized handler """
-    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.before_request
@@ -51,13 +55,14 @@ def authenticate_user():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/',
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
-            if auth_header is None:
+            request.current_user = auth.current_user(request)
+            if auth_header is None and auth.session_cookie(request) is None:
                 abort(401)
-            if user is None:
+            if request.current_user is None:
                 abort(403)
 
 
