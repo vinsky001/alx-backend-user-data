1089ecad59d843da322c765fca88e8f28ce09c0f
vinsky001 <ekibet544@gmail.com>
2023-11-11 09:12:57 +0000

    Added a request for session cookie

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,6 +55,7 @@ def authenticate_user():
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
+            sess_cookie = auth.session_cookie(request)
             request.current_user = auth.current_user(request)
             if auth_header is None and auth.session_cookie(request) is None:
                 abort(401)
