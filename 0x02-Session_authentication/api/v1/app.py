080ca643f78b089a06371167e5d319d48edb6436
vinsky001 <ekibet544@gmail.com>
2023-11-11 10:19:53 +0000

    Session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,11 +56,12 @@ def authenticate_user():
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
             sess_cookie = auth.session_cookie(request)
-            request.current_user = auth.current_user(request)
+            user = auth.current_user(request)
             if auth_header is None and auth.session_cookie(request) is None:
                 abort(401)
             if request.current_user is None:
                 abort(403)
+            request.current_user = auth.current_user(request)    
 
 
 if __name__ == "__main__":
