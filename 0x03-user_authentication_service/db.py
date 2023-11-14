5dcc9eb1f24fb2a065d5de323bfb132132671d3b
vinsky001 <ekibet544@gmail.com>
2023-11-14 10:11:28 +0000

    Implemented   the add_user method, which has two required string arguments: email and hashed_password, and returns a User object.

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> 'User':
+        """
+        Add a user to the database
+        """
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+        except sqlalchemy.exc.IntegrityError as e:
+            # This catches integrity errors
+            self._session.rollback()
+            user = None
+            print(f"Error adding user: {e}")
+        except Exception as e:
+            # Catch more general exceptions for debugging purposes
+            self._session.rollback()
+            user = None
+            return user
