f472aabc00b3b6329b1b38605b8edb54cc361339
vinsky001 <ekibet544@gmail.com>
2023-11-14 11:01:46 +0000

    mplemented the add_user method, which has two required string arguments: email and hashed_password, and returns a User object.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> 'User':
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         Add a user to the database
         """
