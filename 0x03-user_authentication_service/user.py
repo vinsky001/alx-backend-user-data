49c1be5888e920ca0ee6f579ef886cdc23704223
vinsky001 <ekibet544@gmail.com>
2023-11-14 10:06:58 +0000

    SQLAlchemy model named User for a database table named users

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """
-Contains a SQLAlchemy model named User for a database table named users
+Conatains a SQLAlchemy model named User for a database table named users
 """
 
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
