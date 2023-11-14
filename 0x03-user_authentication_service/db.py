9210125e810c98f700f709ece17582233c7c50d1
vinsky001 <ekibet544@gmail.com>
2023-11-14 11:06:06 +0000

    mplemented the add_user method, which has two required string arguments: email and hashed_password, and returns a User object.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -38,7 +38,7 @@ class DB:
             user = User(email=email, hashed_password=hashed_password)
             self._session.add(user)
             self._session.commit()
-        except Exception as e:
+        except Exception:
             # This catches integrity errors
             self._session.rollback()
             user = None
