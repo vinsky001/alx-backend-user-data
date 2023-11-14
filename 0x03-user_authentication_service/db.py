beb22c5950fea610303b3a000e34a695b7fcfe12
vinsky001 <ekibet544@gmail.com>
2023-11-14 10:56:29 +0000

    mplemented the add_user method, which has two required string arguments: email and hashed_password, and returns a User object.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, user
 
 
 class DB:
