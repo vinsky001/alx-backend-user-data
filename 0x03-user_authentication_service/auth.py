2cae32401774b66361393bafcea53181361957a6
vinsky001 <ekibet544@gmail.com>
2023-11-16 06:58:42 +0000

    user auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,6 +46,48 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except Exception:
             return None
-        session_id = _generate_uuid()
+        session_id = self._generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get user based on their session ID"""
+        if session_id:
+            try:
+                user = self._db.find_user_by(session_id=session_id)
+            except Exception:
+                return None
+            return user
+        return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy a user's session ID"""
+        self._db.update_user(user_id, session_id=None)
+        return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate a password reset token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            user = None
+        if user is None:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update the user's password if reset tooken is available in db"""
+        user = None
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except Exception:
+            user = None
+        if user is not None:
+            hashed_password = _hash_password(password)
+            self._db.update_user(
+                user.id, hashed_password=hashed_password,
+                reset_token=None
+            )
+        raise ValueError
