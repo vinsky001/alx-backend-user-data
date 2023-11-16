6cf7ef1fb019e659b41cce32451a86de19ff6d3c
vinsky001 <ekibet544@gmail.com>
2023-11-16 07:52:06 +0000

    Get reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,11 +73,10 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except Exception:
-            user = None
-        if user is None:
-            raise ValueError
-        reset_token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=reset_token)
+            if user is None:
+                raise ValueError
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
 
     def update_password(self, reset_token: str, password: str) -> None:
