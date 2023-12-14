9f257b91f3f45f6f9337f9b6e613ccf6e676e23f
vinsky001 <ekibet544@gmail.com>
2023-12-14 09:11:15 +0000

    Changed value error to "UserDNE"

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,7 +73,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except UserNotFoundError as e:
-            raise ValueError(f"User with email {email} not found.")
+            raise ValueError("User DNE")
         reset_token = str(uuid.uuid4())
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
