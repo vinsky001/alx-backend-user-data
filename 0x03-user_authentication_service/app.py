8ca551585db0f9d2a44949e79346504d29f45267
vinsky001 <ekibet544@gmail.com>
2023-12-14 09:41:16 +0000

    Added a check for a missing email in the form data and returned a 400 status code with an error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -61,12 +61,14 @@ def profile():
 def get_reset_password_token():
     """Get password reset token"""
     email = request.form.get("email")
-    reset_token = None
+    if not email
+    return jsonify({"error": "Email is required"}), 400
+
     try:
         reset_token = AUTH.get_reset_password_token(email)
     except ValueError:
         abort(403)
-    return jsonify({"email": email, "reset_token": reset_token})
+    return jsonify({"Email": email, "reset_token": rest_token}), 200
 
 
 @app.route('/reset_password ', methods=['PUT'], strict_slashes=False)
